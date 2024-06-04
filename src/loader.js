821573289810081324d1981cc740e3f7781c6f91
Claudio Benedetti <claudio.benedetti@mia-platform.eu>
2024-06-04 22:13:21 +0200

    refactor

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,12 @@
 
 const initialize = () => {
   const searchValues = Object.fromEntries(window.location.search.replace(/^\?/, '').split('&').map((chunk) => chunk.split('=')))
+  const base = 'intro-al-codice'
+  const [prefix,] = window.location.pathname.split(base)
 
   if (searchValues.topic && searchValues.exercise) {
-    const exercisePath = `./catalogue/${searchValues.topic}/${searchValues.exercise}.js`
+    const exercisePath = `${prefix}${base}/src/catalogue/${searchValues.topic}/${searchValues.exercise}.js`
+    console.log(prefix, exercisePath)
     const script = document.createElement('script')
     script.setAttribute('src', exercisePath)
     document.body.appendChild(script)
